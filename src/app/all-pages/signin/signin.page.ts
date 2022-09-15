4e6c16e4e60fefe5b42f9e826a6d3cfb92bf5780
akashproject <akashdutta.scriptcrown@gmail.com>
2022-09-15 18:29:18 +0530

    otp value

--- src/app/all-pages/signin/signin.page.ts
+++ src/app/all-pages/signin/signin.page.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewChildren } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Router, NavigationExtras } from '@angular/router';
 import { ApiService } from '../../all-services/api.service';
@@ -11,22 +11,17 @@ import { UtilService } from 'src/app/all-services/util.service';
 })
 export class SigninPage implements OnInit {
 
-  sendOtp = true;
+  sendOtp = false;
   mobileNo : any ;
   userData : any [];
   session_id : any ;
-  @ViewChild("otp1") private otp1: ElementRef<HTMLInputElement>;
-  @ViewChild("otp2") private otp2: ElementRef;
-  @ViewChild("otp3") private otp3: ElementRef;
-  @ViewChild("otp4") private otp4: ElementRef;
-  @ViewChild("otp5") private otp5: ElementRef;
-  @ViewChild("otp6") private otp6: ElementRef;
-  one: String = "";
-  two: String = "";
-  three: String = "";
-  four: String = "";
-  five: String = "";
-  six: String = "";
+  @ViewChild("otp1") otp1:HTMLIonInputElement;
+  @ViewChild('otp2') otp2:HTMLIonInputElement;
+  @ViewChild("otp3") otp3:HTMLIonInputElement;
+  @ViewChild("otp4") otp4:HTMLIonInputElement;
+  @ViewChild("otp5") otp5:HTMLIonInputElement;
+  @ViewChild("otp6") otp6:HTMLIonInputElement;
+  otpValue : any = [];
   otpInput : any ;
   disable: Boolean = true;
   constructor( 
@@ -64,23 +59,21 @@ export class SigninPage implements OnInit {
     
   }
 
+  onPaste(event: ClipboardEvent){
+    let clipboardData = event.clipboardData;
+    let pastedText = clipboardData.getData('text').split("");
+    this.otpValue = pastedText;
+  }
+
   gotoQuote(){
     this.router.navigate(['/quote']);
   }
 
-  _focus() {
-    this.otp1.nativeElement.focus();
-    return true;
-
-  }
-
   matchOtp() {
-    const user_otp = this.one + "" + this.two + "" + this.three + "" + this.four + "" + this.five + "" + this.six;
-
     this.util.presentLoading(); 
     let param = {
       "session_id":this.session_id,
-      "otp_value":user_otp,
+      "otp_value":this.otpValue.join(""),
     }
     this.api.post('api/verify-otp', param).subscribe((data: any) => {
       let reponse = JSON.parse(data)
@@ -96,25 +89,11 @@ export class SigninPage implements OnInit {
       this.util.presentToast("Unable to send OTP! Please try again")
     });
 
-    // if (this.otpValue == user_otp) {
-    //   console.log("equal")
-    //   //this.util.presentToast("");
-    //   this.getUserByMobile();
-    // } else {
-    //   console.log("not equal");      
-    //   this.util.presentToast("The OTP did not match");
-    //   setTimeout(() => {
-    //   this.one = "";
-    //   this.two = "";this.three = "";this.four = "";this.five = "";this.six = "";
-
-    //   },1900);
-      
-    // }
-
   }
 
   gotoNextField(next) {
-    console.log(next);
+    console.log(next.setFocus());
+    
   }
 
   getUserByMobile(){
