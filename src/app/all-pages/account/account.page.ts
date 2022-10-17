fb6ab4a6f28ea6b40134b7e5b76512598ebae6f8
akashproject <akashdutta.scriptcrown@gmail.com>
2022-10-17 15:23:53 +0530

    logout update

--- src/app/all-pages/account/account.page.ts
+++ src/app/all-pages/account/account.page.ts
@@ -3,6 +3,7 @@ import { Location } from '@angular/common';
 import { Router, NavigationExtras } from '@angular/router';
 import { ApiService } from '../../all-services/api.service';
 import { UtilService } from 'src/app/all-services/util.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-account',
@@ -12,11 +13,13 @@ import { UtilService } from 'src/app/all-services/util.service';
 export class AccountPage {
 
   hasUser = false;
+  copywriteDate = new Date('2011-02-02T08:00:00Z')
   constructor( 
     public api: ApiService,
     public router: Router,
     private location:Location,
-    private util:UtilService
+    private util:UtilService,
+    private alertController: AlertController,
   ) {
     
   }
@@ -32,9 +35,35 @@ export class AccountPage {
 
   }
 
+  async logoutAlert() {
+    
+    const alert = await this.alertController.create({
+      header: 'Logout to Device?',
+      message: 'Are you sure to logout from your account',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            
+          },
+        },
+        {
+          text: 'OK',
+          role: 'confirm',
+          handler: () => {
+            this.logout()
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 
   logout(){
     localStorage.clear();
-    this.router.navigate(['/']);
+    this.util.presentToast("You have successfuly logged out form your account")
+    window.location.reload();
   }
 }
